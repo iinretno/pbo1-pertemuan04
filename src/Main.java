2ad5dd743a19eea0c2ca128293f48cc4b9d552d6
iinretno <133147886+iinretno@users.noreply.github.com>
2023-05-18 20:04:11 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+
+        do {
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }while(isiGelas != penuh)
+
+        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+
+    }
+}
